75f6f2c142829a2366b3303127f4ece9a4d95508
muhammad77akbarr <muhammad77akbarr@gmail.com>
2025-10-13 09:37:06 +0800

    ADD Form Login

--- LatihanGithub/FormLogin.Designer.cs
+++ LatihanGithub/FormLogin.Designer.cs
@@ -28,12 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(30, 33);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(63, 15);
+            this.lblUsername.TabIndex = 0;
+            this.lblUsername.Text = "Username:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(30, 73);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(60, 15);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(110, 30);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(200, 23);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(110, 70);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(200, 23);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(110, 110);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(200, 30);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(110, 155);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 15);
+            this.lblStatus.TabIndex = 5;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(350, 200);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
--- LatihanGithub/FormLogin.cs
+++ LatihanGithub/FormLogin.cs
@@ -1,10 +1,117 @@
+using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
 namespace LatihanGithub
 {
     public partial class FormLogin : Form
     {
+        // Connection string untuk MySQL
+        private string connectionString = "server=localhost;database=db_github;uid=root;pwd=;";
+
         public FormLogin()
         {
             InitializeComponent();
+
+            // Set Enter key untuk pindah ke kontrol berikutnya
+            this.txtUsername.KeyPress += TxtUsername_KeyPress;
+            this.txtPassword.KeyPress += TxtPassword_KeyPress;
+        }
+
+        private void TxtUsername_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                txtPassword.Focus();
+                e.Handled = true;
+            }
+        }
+
+        private void TxtPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                btnLogin.PerformClick();
+                e.Handled = true;
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text;
+
+            // Validasi input
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+                lblStatus.Text = "Username dan Password wajib diisi.";
+                return;
+            }
+
+            // Disable button saat proses login
+            btnLogin.Enabled = false;
+            lblStatus.ForeColor = System.Drawing.Color.Blue;
+            lblStatus.Text = "Memproses login...";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    // Query untuk mengecek login
+                    string query = "SELECT COUNT(*) FROM login WHERE username = @username AND password = @password";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@username", username);
+                        cmd.Parameters.AddWithValue("@password", password);
+
+                        int result = Convert.ToInt32(cmd.ExecuteScalar());
+
+                        if (result > 0)
+                        {
+                            lblStatus.ForeColor = System.Drawing.Color.Green;
+                            lblStatus.Text = "Login berhasil!";
+
+                            // Beri sedikit delay sebelum berpindah form
+                            System.Threading.Thread.Sleep(500);
+
+                            // Lanjut ke form berikutnya
+                            this.Hide();
+                            FormMenu mainForm = new FormMenu();
+                            mainForm.Show();
+                        }
+                        else
+                        {
+                            lblStatus.ForeColor = System.Drawing.Color.Red;
+                            lblStatus.Text = "Username atau Password salah.";
+                            txtPassword.Clear();
+                            txtPassword.Focus();
+                        }
+                    }
+                }
+                catch (MySqlException sqlEx)
+                {
+                    lblStatus.ForeColor = System.Drawing.Color.Red;
+                    lblStatus.Text = "Error Database: " + sqlEx.Message;
+                    MessageBox.Show("Gagal terhubung ke database MySQL.\n" + sqlEx.Message,
+                                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    lblStatus.ForeColor = System.Drawing.Color.Red;
+                    lblStatus.Text = "Error: " + ex.Message;
+                    MessageBox.Show("Terjadi kesalahan: " + ex.Message,
+                                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    btnLogin.Enabled = true;
+                }
+            }
         }
     }
 }
